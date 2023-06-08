ad1b4255881d9df00b0367df81375c4a893772fb
Evolutionis <lftthomaz@outlook.com>
2023-06-08 18:39:19 -0300

    feat: criando rotas utilizando o recurso lazyload

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'registers' },
+  {
+    path: 'registers',
+    loadChildren: () => import('./pages/registers/registers.module')
+      .then(m => m.RegistersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
