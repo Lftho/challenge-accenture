0547abba1214101a8665c4ab752d5c75f315c61d
Evolutionis <lftthomaz@outlook.com>
2023-06-11 19:33:19 -0300

    fix: Erro ao importar como class e o correto é interface

--- backend/src/main/java/com/luiz/backend/repository/EmpresaRepository.java
+++ backend/src/main/java/com/luiz/backend/repository/EmpresaRepository.java
@@ -6,4 +6,4 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public class EmpresaRepository extends JpaRepository<Empresa, String>{}
+public interface EmpresaRepository extends JpaRepository<Empresa, String>{}
