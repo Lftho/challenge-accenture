f580d7adcf912a65d2ca1476a2c3a0b8074919cf
Evolutionis <lftthomaz@outlook.com>
2023-06-12 12:28:06 -0300

    chore: subindo as funções listAll e o create

--- backend/src/main/java/com/luiz/backend/controller/EmpresaController.java
+++ backend/src/main/java/com/luiz/backend/controller/EmpresaController.java
@@ -3,8 +3,14 @@ package com.luiz.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luiz.backend.model.Empresa;
@@ -21,6 +27,13 @@ public class EmpresaController {
   
   @GetMapping
   public List<Empresa> listAll() {
-    return null;
+    return empresaRepository.findAll();
+  }
+
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Empresa create(@RequestBody Empresa empresa) {
+    return empresaRepository.save(empresa);
   }
 }
