f843b4c63be8e7e2d0f492b33daceb80b7a421b2
Evolutionis <lftthomaz@outlook.com>
2023-06-12 12:27:23 -0300

    chore: ajustes no create e validação de erros

--- web/src/app/pages/registers/register-form/register-form.component.ts
+++ web/src/app/pages/registers/register-form/register-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { RegistersService } from 'src/app/shared/services/registers.service';
 
 @Component({
@@ -13,7 +15,9 @@ export class RegisterFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private registersService: RegistersService
+    private registersService: RegistersService,
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       validate: [null],
@@ -28,10 +32,27 @@ export class RegisterFormComponent implements OnInit {
 
   onSubmit() {
     this.registersService.save(this.form.value)
-      .subscribe(data => {
-        console.log('sucesso', data);
-      });
+      .subscribe(data =>
+        this.onSuccess(),
+        error => this.onError()
+      );
   }
 
-  onCancel() { }
+  onCancel() {
+    this.location.back();
+  }
+
+
+  private onSuccess() {
+    this._snackBar.open('Registro salvo com sucesso', 'fechar', {
+      duration: 3000
+    });
+    this.onCancel();
+  }
+
+  private onError() {
+    this._snackBar.open('Erro ao salvar um registro', 'fechar', {
+      duration: 3000
+    });
+  }
 }
