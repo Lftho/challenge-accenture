a10eb5c8c41b36a2dc2cf0f47da3c4c50eedf61a
Evolutionis <lftthomaz@outlook.com>
2023-06-12 12:26:19 -0300

    chore: update para as variaveis

--- backend/src/main/java/com/luiz/backend/model/Empresa.java
+++ backend/src/main/java/com/luiz/backend/model/Empresa.java
@@ -1,8 +1,11 @@
 package com.luiz.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+
 import lombok.Data;
 
 
@@ -12,9 +15,11 @@ public class Empresa {
   
   @Id
   @Column (length = 14, nullable = false)
+  @JsonProperty("cnpjOfCpf")
   private String cnpj;
 
   @Column (length = 30, nullable = false)
+  @JsonProperty("name")
   private String nomeFantasia;
 
   @Column (length = 8, nullable = false)
